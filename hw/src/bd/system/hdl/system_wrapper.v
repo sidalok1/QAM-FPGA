11c91e6b79f0ce9d5e05a3d9331224598fade1aa
Sidharth Alok Girdhar <116860545+sidalok1@users.noreply.github.com>
2026-04-23 00:16:12 -0700

    Interrupt support for microblaze hardware The microblaze software is less buggy and slightly more responsive, though there are many improvements to be had in the terminal app, which is somewhat over-engineered right now.

--- hw/src/bd/system/hdl/system_wrapper.v
+++ hw/src/bd/system/hdl/system_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2025.1 (win64) Build 6140274 Thu May 22 00:12:29 MDT 2025
-//Date        : Wed Apr 22 14:27:59 2026
+//Date        : Wed Apr 22 22:52:45 2026
 //Host        : SID3 running 64-bit major release  (build 9200)
 //Command     : generate_target system_wrapper.bd
 //Design      : system_wrapper
--- sw/main.c
+++ sw/main.c
@@ -1,81 +1,159 @@
+#ifndef __MICROBLAZE__
+#define __MICROBLAZE__
+#endif
+
+#include <xil_assert.h>
 #include "xparameters.h"
 #include "mb_interface.h"
 #include "xil_types.h"
 #include "xuartlite.h"
-#include "xil_printf.h"
+#include "xuartlite_l.h"
 #include "xstatus.h"
+#include "xintc_l.h"
+#include "xintc.h"
+#include "xil_exception.h"
+#include "xil_printf.h"
 #include <string.h>
 
-XUartLite UartLite;
+XUartLite uart;
+static XIntc interrupt_controller;
+XIntc *intc = &interrupt_controller;
 
 #define RADIO_FSL_ID 0
-#define MAX_BYTES 2048
+#define MAX_BYTES 128
 #define MSR_FSL_BIT 0x10
 
-u8 uart_recv_buf[MAX_BYTES];
-unsigned int uart_recv_idx = 0;
-u8 radio_recv_buf[MAX_BYTES];
-unsigned int radio_recv_idx = 0;
+#define RADIO_INT_ID 0
 
-int init_sys();
-void shd_sys();
+u8 uart_recv_buf[MAX_BYTES];
+u8 uart_recv_idx = 0;
+int uart_empty = TRUE;
+u8 radio_recv_buf[MAX_BYTES];
+u8 radio_recv_idx = 0;
+
+int init();
+// Source code comments say I need to add this attribute, though I'm not sure how necessary this is,
+// as I cannot find example code using fast interrupts
+void read_from_uart() __attribute__ ((fast_interrupt)); // must check why interrupt was signalled
+void read_from_radio() __attribute__ ((fast_interrupt));
 void send_over_radio();
-int send_over_uart();
-int radio_recv();
+void send_over_uart();
 
 int main() {
     int status;
-    if ( (status = init_sys()) != XST_SUCCESS ) return status;
+    if ((status = init()) != XST_SUCCESS) return status;
+    while (1) send_over_uart();
+}
+
+
+int init() {
+    int status;
+    
+
+    // Initialize interrupt controller
+    status = XIntc_Initialize(intc, XPAR_XINTC_0_BASEADDR);
+    if (status != XST_SUCCESS) return XST_FAILURE;
+    // Interrupt controller self test
+    status = XIntc_SelfTest(intc);
+    if (status != XST_SUCCESS) return XST_FAILURE;
+    // Connect the interrupt handlers to the interrupt controller
+    // - Connect radio rx interrupt handler
+    status = XIntc_ConnectFastHandler(intc, XPAR_FABRIC_AXI_UARTLITE_0_INTR, (XFastInterruptHandler) read_from_uart);
+    if (status != XST_SUCCESS) return XST_FAILURE;
+    // - Connect uart rx interrupt handler (custom, not supplied by uart driver)
+    status = XIntc_ConnectFastHandler(intc, RADIO_INT_ID, (XFastInterruptHandler) read_from_radio);
+    if (status != XST_SUCCESS) return XST_FAILURE;
+    // Initialize uart
+    status = XUartLite_Initialize(&uart, XPAR_AXI_UARTLITE_0_BASEADDR);
+    if (status != XST_SUCCESS) return XST_FAILURE;
+    // Uart self test
+    status = XUartLite_SelfTest(&uart);
+    if (status != XST_SUCCESS) return XST_FAILURE;
+    // Enable exceptions. I'm not really sure if this is needed. I think using fast interrupt handlers it is not, but
+    // I can't find any example code using fast interrupts.
+    Xil_ExceptionInit();
+    Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler) XIntc_DeviceInterruptHandler, (void *)0);
+    Xil_ExceptionEnable();
+
     print("Done with startup\n");
     strcpy((char*)uart_recv_buf, "init\n");
     uart_recv_idx = 4;
+    uart_empty = FALSE;
     send_over_radio();
-    while (1) {
-        if ( XUartLite_Recv(&UartLite, uart_recv_buf + uart_recv_idx, 1) == 1 ) {
-            switch ( uart_recv_buf[uart_recv_idx] ) {
-            case '\010':
-                uart_recv_idx--;
-            break;
-            case '\r':
-            case '\n':
-                send_over_radio();
-            break;
-            default:
-                uart_recv_idx++;
-            break;
-            }
-        }
-        if ( (status = radio_recv()) != XST_SUCCESS ) return status;
-    }
+    
+    
+    // Enable interrupts
+    XIntc_Enable(intc, XPAR_FABRIC_AXI_UARTLITE_0_INTR);
+    XUartLite_EnableInterrupt(&uart);
+    XIntc_Enable(intc, RADIO_INT_ID);
 
-    shd_sys();
+    
+    // Start interrupt handling
+    return XIntc_Start(intc, XIN_REAL_MODE);
 
-    return XST_SUCCESS;
 }
 
-int radio_recv() {
-    u32 w, s;
-    u8 c;
-    ngetfsl(w, RADIO_FSL_ID);
-    fsl_isinvalid(s);
-    while ( !s ) {
-        c = w & 0xFF;
-        radio_recv_buf[radio_recv_idx++] = c;
-        fsl_iserror(s); // indicates control (tlast asserted)
-        if ( s ) {
-            msrclr(MSR_FSL_BIT);
-            return send_over_uart();
+void read_from_uart() {
+    // According to the MicroBlaze documentation, the MicroBlaze itself will automatically
+    // disable and enable (all) interrupts
+
+    // while there is data to be received over uart
+    while (!XUartLite_IsReceiveEmpty(XPAR_AXI_UARTLITE_0_BASEADDR)) {
+        // read a byte from the uart fifo
+        u8 byte = XUartLite_ReadReg(XPAR_AXI_UARTLITE_0_BASEADDR, XUL_RX_FIFO_OFFSET);
+        switch (byte) {
+        case '\010': // backspace
+            uart_recv_idx--;
+            break;
+        case '\r':
+        case '\n': // enter
+            uart_recv_buf[uart_recv_idx] = byte;
+            send_over_radio(); // may set uart_empty = TRUE
+            break;
+        default: // all other characters considered valid (as of now)
+            uart_recv_buf[uart_recv_idx++] = byte;
+            uart_empty = FALSE;
+            break;
         }
-        ngetfsl(w, RADIO_FSL_ID);
-        fsl_isinvalid(s);
     }
-    return XST_SUCCESS;
+    // It is possible for this handler to be called when the uart tx fifo is empty, regardless
+    // of whether or not the uart rx fifo contains data.
+    // Because the main loop already handles sending data over uart, no need to call it here
+    return;
+}
+
+void send_over_uart() {
+    u8 idx = 0;
+    while (idx != radio_recv_idx) {
+        XUartLite_SendByte(XPAR_XUARTLITE_0_BASEADDR, radio_recv_buf[idx++]);
+    }
+    radio_recv_idx = 0;
+}
+
+void read_from_radio() {
+    u32 w, s; // data word, and status register
+    u8 c; // data byte
+    // Only called if radio requests an interrupt, which should only happen
+    // if there is data to be received. Moreover, interrupts will not be requested again,
+    // so reads should happen in blocking mode
+    do {
+        // place stream link data into word
+        getfsl(w, RADIO_FSL_ID);
+        // extract least significant byte
+        c = w & 0xFF;
+        // add to the radio receive buffer
+        radio_recv_buf[radio_recv_idx++] = c;
+        // will be set if tlast is asserted
+        fsl_iserror(s);
+    } while (!s);
+    msrclr(MSR_FSL_BIT);
 }
 
 void send_over_radio() {
     u8 c;
     u32 w;
-    unsigned int i;
+    u8 i;
+    if (uart_empty) return;
     for ( i = 0; i < uart_recv_idx; i++ ) {
         c = uart_recv_buf[i];
         w = c & 0xFF;
@@ -85,28 +163,5 @@ void send_over_radio() {
     w = c &0xFF;
     cputfsl(w, RADIO_FSL_ID);
     uart_recv_idx = 0;
-}
-
-int send_over_uart() {
-    if ( XUartLite_Send(&UartLite, radio_recv_buf, radio_recv_idx) != radio_recv_idx )
-        return XST_SEND_ERROR;
-    radio_recv_idx = 0;
-    return XST_SUCCESS;
-}
-
-int init_sys() {
-    int status;
-    microblaze_enable_dcache();
-    microblaze_enable_icache();
-    status = XUartLite_Initialize(&UartLite, XPAR_AXI_UARTLITE_0_BASEADDR);
-    status |= XUartLite_SelfTest(&UartLite);
-    if ( status != XST_SUCCESS ) {
-        return XST_FAILURE;
-    }
-    return XST_SUCCESS;
-}
-
-void shd_sys() {
-    microblaze_disable_dcache();
-    microblaze_disable_icache();
+    uart_empty = TRUE;
 }
