47b15f9f247c49a4a66f9936b4aa11818e8338fe
Sidharth Alok Girdhar <116860545+sidalok1@users.noreply.github.com>
2026-05-06 21:34:01 -0700

    Removed unused files The design presently no longer interfaces with a microblaze, instead directly communicating over uart. Since there is no microblaze, there is no block design and since there is no block design there is no need to carry around project files.

--- matlab/modulate_carrier.m
+++
@@ -1,5 +0,0 @@
-function signal = modulate_carrier(transmited_baseband, Fc, t, phase_offset)
-    carrier = exp(1j * (2 * pi * Fc * t + phase_offset));
-    analytic = transmited_baseband .* carrier;
-    signal = real(analytic);
-end
